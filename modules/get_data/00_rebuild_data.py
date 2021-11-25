12f88a31ab2bdf8ee3e10609ebbe9f93c0269871
Tony Mele <TonyMele.Rutgers@gmail.com>
2021-11-25 02:09:02 -0500

    fixing

--- modules/get_data/00_rebuild_data.py
+++ modules/get_data/00_rebuild_data.py
@@ -2,6 +2,7 @@ from a_id_files import get_file_list as A
 from b_extract_relevant_rows import download_raw_tsvs as B
 from c_files_to_pandas import create_data_frames as C
 
+
 def main():
     if __name__== "__main__" :
         input("Download Files?")
